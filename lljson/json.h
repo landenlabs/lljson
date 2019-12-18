af6532cc0e8630a91da3e06b45489d9234a39bb1
landenlabs2 <landenlabs2@gmail.com>
2019-12-18 00:10:12 -0500

    first commit

---
+++ lljson/json.h
@@ -0,0 +1,208 @@
+//-------------------------------------------------------------------------------------------------
+//
+// File: json.h   Author: Dennis Lang  Desc: Parse json
+//
+//-------------------------------------------------------------------------------------------------
+//
+// Author: Dennis Lang - 2019
+// http://landenlabs.com
+//
+// This file is part of lljson project.
+//
+// ----- License ----
+//
+// Copyright (c) 2019 Dennis Lang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#ifndef json_h
+#define json_h
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <regex>
+#include <exception>
+using namespace std;
+
+typedef std::vector<lstring> StringList;
+typedef std::map<string, StringList> MapList;
+
+
+class JsonBase {
+public:
+    enum Jtype { None, Value, Array, Map };
+    Jtype mJtype = None;
+    JsonBase(Jtype jtype) {
+        mJtype = jtype;
+    }
+    
+    virtual
+    string toString() = 0;
+    
+    virtual
+    ostream& dump(ostream& out) = 0;
+    
+    virtual
+    void toMapList(MapList& mapList, string key) = 0;
+};
+
+typedef std::vector<JsonBase*> VecJson;
+typedef std::map<string, JsonBase*> MapJson;
+
+class JsonValue : public JsonBase, public string {
+public:
+    JsonValue(const char* str) : JsonBase(Value), string(str) {
+    }
+    JsonValue(string& str) : JsonBase(Value), string(str) {
+    }
+    
+    ostream& dump(ostream& out) {
+        out << toString();
+        return out;
+    }
+    
+    void toMapList(MapList& mapList, string key)  {
+        cerr << toString();
+    }
+    
+    string toString() {
+        return *this;
+    }
+};
+
+class JsonArray : public JsonBase, public VecJson {
+public:
+    JsonArray() : JsonBase(Array) {
+    }
+    
+    string toString() {
+        std::ostringstream ostr;
+        ostr << "[\n";
+        JsonArray::iterator it = begin();
+        bool addComma = false;
+        while (it != end())
+        {
+            if (addComma)
+                ostr << ",\n";
+            addComma = true;
+            (*it++)->dump(ostr);
+        }
+        ostr << "\n]";
+        return ostr.str();
+    }
+    
+    ostream& dump(ostream& out) {
+        cout << toString();
+        return cout;
+    }
+    
+    void toMapList(MapList& mapList, string key)  {
+        StringList& list = mapList[key];
+        JsonArray::iterator it = begin();
+        while (it != end())
+        {
+            list.push_back((*it++)->toString());
+        }
+    }
+};
+class JsonMap : public JsonBase, public MapJson {
+public:
+    JsonMap() : JsonBase(Map), MapJson() {
+    }
+    
+    string toString() {
+        ostringstream out;
+        out << "{\n";
+        JsonMap::iterator it = begin();
+        bool addComma = false;
+        while (it != end())
+        {
+            if (addComma)
+                out << ",\n";
+            addComma = true;
+            
+            std::string name = it->first;
+            JsonBase* pValue = it->second;
+            cout << name << ": ";
+            pValue->dump(out);
+            it++;
+        }
+        out << "\n}\n";
+        return out.str();
+    }
+    ostream& dump(ostream& out) {
+        out << toString();
+        return out;
+    }
+    
+    void toMapList(MapList& mapList, string key)  {
+        JsonMap::iterator it = begin();
+        while (it != end())
+        {
+            std::string name = it->first;
+            JsonBase* pValue = it->second;
+            pValue->toMapList(mapList, name);
+            it++;
+        }
+    }
+};
+
+typedef JsonMap JsonFields;
+
+class JsonBuffer : public std::vector<char> {
+public:
+    char keyBuf[10];
+    
+    size_t pos = 0;
+    int seq = 100;
+    char nextChr() {
+        if (pos < size()) {
+            return at(pos++);
+        }
+        return NULL;
+    }
+    
+    string nextKey() {
+        snprintf(keyBuf, sizeof(keyBuf), "%03d", seq++);
+        return *(new string(keyBuf));
+    }
+    
+    const char* ptr(int len = 0) {
+        const char* nowPtr = &at(pos);
+        pos = std::min(pos + len, size());
+        return nowPtr;
+    }
+};
+
+class JsonToken : public string {
+public:
+    enum Token { Value, EndArray, EndGroup, EndParse } ;
+    Token mToken = Value;
+    JsonToken() : string() {
+    }
+    JsonToken(const char* str) : string(str) {
+    }
+    JsonToken(Token token) : string(), mToken(token) {
+    }
+};
+static JsonToken END_ARRAY(JsonToken::EndArray);
+static JsonToken END_GROUP(JsonToken::EndGroup);
+static JsonToken END_PARSE(JsonToken::EndParse);
+
+
+#endif /* json_h */
---
+++ lljson/lljson.cpp
@@ -0,0 +1,479 @@
+//-------------------------------------------------------------------------------------------------
+//
+//  lljson      Dec-2019       Dennis Lang
+//
+//  Convert json to csv (tanspose arrays into columns)
+//
+// Example input json:
+//  {
+//     "cloudCover": [
+//       10,
+//       30,
+//       49
+//     ],
+//       "dayOfWeek": [
+//       "Monday",
+//       "Tuesday",
+//       "Wednesday"
+//     ]
+//  }
+//
+//  Output trasnposed CSV
+//   cloudCover,  dayOfWeek
+//    10, Monday
+//    30, Tuesday
+//    49, WednesDay
+//
+//-------------------------------------------------------------------------------------------------
+//
+// Author: Dennis Lang - 2019
+// http://landenlabs.com/
+//
+// This file is part of lljson project.
+//
+// ----- License ----
+//
+// Copyright (c) 2016 Dennis Lang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// 4291 - No matching operator delete found
+#pragma warning( disable : 4291 )
+
+#include <stdio.h>
+#include <ctype.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+
+#include "ll_stdhdr.h"
+#include "directory.h"
+#include "split.h"
+#include "json.h"
+
+
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <regex>
+#include <exception>
+using namespace std;
+
+// Helper types
+typedef std::vector<std::regex> PatternList;
+typedef unsigned int uint;
+
+
+// Runtime options
+PatternList includeFilePatList;
+PatternList excludeFilePatList;
+StringList fileDirList;
+bool showFile = true;
+
+uint optionErrCnt = 0;
+uint patternErrCnt = 0;
+
+
+#ifdef WIN32
+const char SLASH_CHAR('\\');
+#else
+const char SLASH_CHAR('/');
+#endif
+
+// ---------------------------------------------------------------------------
+// Extract name part from path.
+lstring& getName(lstring& outName, const lstring& inPath)
+{
+    size_t nameStart = inPath.rfind(SLASH_CHAR) + 1;
+    if (nameStart == 0)
+        outName = inPath;
+    else
+        outName = inPath.substr(nameStart);
+    return outName;
+}
+
+// ---------------------------------------------------------------------------
+// Return true if inPath (filename part) matches pattern in patternList
+bool FileMatches(const lstring& inName, const PatternList& patternList, bool emptyResult)
+{
+    if (patternList.empty() || inName.empty())
+        return emptyResult;
+    
+    for (size_t idx = 0; idx != patternList.size(); idx++)
+        if (std::regex_match(inName.begin(), inName.end(), patternList[idx]))
+            return true;
+    
+    return false;
+}
+
+// ---------------------------------------------------------------------------
+static void getWord( JsonBuffer& buffer, char delim, string& word) {
+    
+    const char* lastPtr = strchr(buffer.ptr(), delim);
+    word.clear();
+    int len = int(lastPtr - buffer.ptr());
+    word.append(buffer.ptr(len+1), len);
+}
+
+static JsonToken parseJson(JsonBuffer& buffer, JsonFields& jsonFields);
+
+// ---------------------------------------------------------------------------
+static void getArray(JsonBuffer& buffer, JsonArray& array) {
+    JsonFields jsonFields;
+    for(;;) {
+        JsonToken token = parseJson(buffer, jsonFields);
+        if (token.mToken == JsonToken::Value) {
+            JsonValue* jsonValue = new JsonValue(token);
+            array.push_back(jsonValue);
+        } else {
+            return;
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
+static void getGroup(JsonBuffer& buffer, JsonFields& fields) {
+    
+    for(;;) {
+        JsonToken token = parseJson(buffer, fields);
+        if (token.mToken == JsonToken::EndGroup) {
+            return;
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
+static JsonToken parseJson(JsonBuffer& buffer, JsonFields& jsonFields) {
+    
+    string fieldName;
+    JsonToken fieldValue;
+    
+    while (buffer.pos < buffer.size()) {
+        char chr = buffer.nextChr();
+        switch (chr) {
+            default:
+                fieldValue += chr;
+                break;
+                
+            case ' ':
+            case '\t':
+            case '\n':
+            case '\r':
+                break;
+            case ',':
+                return fieldValue;
+                
+            case ':':
+                fieldName = fieldValue;
+                fieldValue.clear();
+                break;
+                
+            case '{':
+            {
+                JsonFields* pJsonFields = new JsonFields();
+                jsonFields[buffer.nextKey()] = pJsonFields;
+                getGroup(buffer, *pJsonFields);
+            }
+                break;
+            case '}':
+                return END_GROUP;
+                
+            case '"':
+                getWord(buffer, '"', fieldValue);
+                break;
+            case '[':
+            {
+                JsonArray* pJsonArray = new JsonArray();
+                jsonFields[fieldName] = pJsonArray;
+                getArray(buffer, *pJsonArray);
+            }
+                break;
+            case ']':
+                return END_ARRAY;
+        }
+    }
+    
+    return END_PARSE;
+}
+
+
+// ---------------------------------------------------------------------------
+void JsonDump(JsonBase& base, ostream& out) {
+    base.dump(out);
+}
+
+// ---------------------------------------------------------------------------
+void JsonTranspose(JsonBase& base, ostream& out) {
+    MapList mapList;
+    base.toMapList(mapList, "");
+    
+    MapList::iterator it = mapList.begin();
+    bool addComma = false;
+    size_t maxRows = 0;
+    while (it != mapList.end()) {
+        if (addComma) cout << ", ";
+        addComma = true;
+        
+        out << it->first;
+        maxRows = std::max(maxRows, it->second.size());
+        it++;
+    }
+    out << std::endl;
+    
+    
+    for (int row=0; row < maxRows; row++) {
+        addComma = false;
+        for (it = mapList.begin(); it != mapList.end(); it++) {
+            if (addComma) cout << ", ";
+            addComma = true;
+            out << it->second.at(row);
+        }
+        out << std::endl;
+    }
+    out << std::endl;
+}
+
+
+// ---------------------------------------------------------------------------
+bool InspectFile(const lstring& filepath, const lstring& filename)
+{
+    ifstream        in;
+    ofstream        out;
+    struct stat     filestat;
+    JsonFields fields;
+    
+    try {
+        if (stat(filepath, &filestat) != 0)
+            return false;
+        
+        in.open(filepath);
+        if (in.good())
+        {
+            JsonBuffer buffer;
+            buffer.resize(filestat.st_size+1);
+            streamsize inCnt = in.read(buffer.data(), buffer.size()).gcount();
+            assert(inCnt < buffer.size());
+            in.close();
+            buffer.push_back('\0');
+            
+            
+            parseJson(buffer, fields);
+        }
+        else
+        {
+            cerr << strerror(errno) << ", Unable to open " << filepath << endl;
+        }
+    }
+    catch (exception ex)
+    {
+        cerr << ex.what() << ", Error in file:" << filepath << endl;
+    }
+    
+    cerr << "Json fields=" << fields.size() << std::endl;
+    // JsonDump(fields, cout);
+    JsonTranspose(fields, cout);
+    
+    return false;
+}
+
+
+// ---------------------------------------------------------------------------
+static size_t InspectFile(const lstring& fullname)
+{
+    size_t fileCount = 0;
+    lstring name;
+    getName(name, fullname);
+    
+    if (!name.empty()
+        && !FileMatches(name, excludeFilePatList, false)
+        && FileMatches(name, includeFilePatList, true))
+    {
+        if (InspectFile(fullname, name))
+        {
+            fileCount++;
+            if (showFile)
+                std::cout << fullname << std::endl;
+        }
+    }
+    
+    return fileCount;
+}
+
+// ---------------------------------------------------------------------------
+static size_t InspectFiles(const lstring& dirname)
+{
+    Directory_files directory(dirname);
+    lstring fullname;
+    
+    size_t fileCount = 0;
+    
+    struct stat filestat;
+    try {
+        if (stat(dirname, &filestat) == 0 && S_ISREG(filestat.st_mode))
+        {
+            fileCount += InspectFile(dirname);
+        }
+    }
+    catch (exception ex)
+    {
+        // Probably a pattern, let directory scan do its magic.
+    }
+    
+    while (directory.more())
+    {
+        directory.fullName(fullname);
+        if (directory.is_directory())
+        {
+            fileCount += InspectFiles(fullname);
+        }
+        else if (fullname.length() > 0)
+        {
+            fileCount += InspectFile(fullname);
+        }
+    }
+    
+    return fileCount;
+}
+
+
+// ---------------------------------------------------------------------------
+// Return compiled regular expression from text.
+std::regex getRegEx(const char* value)
+{
+    try {
+        std::string valueStr(value);
+        return std::regex(valueStr);
+        // return std::regex(valueStr, regex_constants::icase);
+    }
+    catch (const std::regex_error& regEx)
+    {
+        std::cerr << regEx.what() << ", Pattern=" << value << std::endl;
+    }
+    
+    patternErrCnt++;
+    return std::regex("");
+}
+
+
+// ---------------------------------------------------------------------------
+// Validate option matchs and optionally report problem to user.
+bool ValidOption(const char* validCmd, const char* possibleCmd, bool reportErr = true)
+{
+    // Starts with validCmd else mark error
+    size_t validLen = strlen(validCmd);
+    size_t possibleLen = strlen(possibleCmd);
+    
+    if ( strncasecmp(validCmd, possibleCmd, std::min(validLen, possibleLen)) == 0)
+        return true;
+    
+    if (reportErr)
+    {
+        std::cerr << "Unknown option:'" << possibleCmd << "', expect:'" << validCmd << "'\n";
+        optionErrCnt++;
+    }
+    return false;
+}
+
+
+// ---------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{  
+    if (argc == 1)
+    {
+        cerr << "\n" << argv[0] << "  Dennis Lang v1.1 (landenlabs.com) " __DATE__ << "\n"
+        << "\nDes: Json parse and output as transposed CSV\n"
+        "Use: lljson [options] directories...\n"
+        "\n"
+        "\n";
+    }
+    else
+    {
+        bool doParseCmds = true;
+        string endCmds = "--";
+        for (int argn = 1; argn < argc; argn++)
+        {
+            if (*argv[argn] == '-' && doParseCmds)
+            {
+                lstring argStr(argv[argn]);
+                Split cmdValue(argStr, "=", 2);
+                if (cmdValue.size() == 2)
+                {
+                    lstring cmd = cmdValue[0];
+                    lstring value = cmdValue[1];
+                    
+                    switch (cmd[1])
+                    {
+                        case 'i':   // includeFile=<pat>
+                            if (ValidOption("includefile", cmd+1))
+                            {
+                                ReplaceAll(value, "*", ".*");
+                                includeFilePatList.push_back(getRegEx(value));
+                            }
+                            break;
+                        case 'e':   // excludeFile=<pat>
+                            if (ValidOption("excludefile", cmd+1))
+                            {
+                                ReplaceAll(value, "*", ".*");
+                                excludeFilePatList.push_back(getRegEx(value));
+                            }
+                            break;
+                            
+                        default:
+                            std::cerr << "Unknown command " << cmd << std::endl;
+                            optionErrCnt++;
+                            break;
+                    }
+                } else {
+                    if (endCmds == argv[argn]) {
+                        doParseCmds = false;
+                    } else {
+                        std::cerr << "Unknown command " << argStr << std::endl;
+                        optionErrCnt++;
+                    }
+                }
+            }
+            else
+            {
+                // Store file directories
+                fileDirList.push_back(argv[argn]);
+            }
+        }
+        
+        if (patternErrCnt == 0 && optionErrCnt == 0 && fileDirList.size() != 0)
+        {
+            if (fileDirList.size() == 1 && fileDirList[0] == "-") {
+                string filePath;
+                while (std::getline(std::cin, filePath)) {
+                    std::cerr << "File Matches=" << InspectFiles(filePath) << std::endl;
+                }
+            } else {
+                for (auto const& filePath : fileDirList)
+                {
+                    std::cerr << "File Matches=" << InspectFiles(filePath) << std::endl;
+                }
+            }
+        }
+        
+        std::cerr << std::endl;
+    }
+    
+    return 0;
+}
