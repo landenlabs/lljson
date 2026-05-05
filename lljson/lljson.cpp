5f09d3cd74cb8a1b4a12ed5adfc84180a95a81cd
Dennis Lang <landenlabs2@gmail.com>
2026-05-05 12:53:38 -0700

    new

--- lljson/lljson.cpp
+++ lljson/lljson.cpp
@@ -33,7 +33,7 @@
 //
 // ----- License ----
 //
-// Copyright (c) 2016 Dennis Lang
+// Copyright (c) 2026 Dennis Lang
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -54,7 +54,7 @@
 
 // 4291 - No matching operator delete found
 #pragma warning( disable : 4291 )
-#define VERSION "v6.05.01"
+#define VERSION "v6.05.04"
 
 // Project files
 #include "ll_stdhdr.hpp"
